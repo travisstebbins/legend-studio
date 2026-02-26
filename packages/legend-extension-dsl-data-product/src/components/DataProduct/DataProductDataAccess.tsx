92db8c955114b25b06b753642eada1e09e2bcf07
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2026-02-26 17:31:04 -0500

    refactor: Rename props

--- packages/legend-extension-dsl-data-product/src/components/DataProduct/DataProductDataAccess.tsx
+++ packages/legend-extension-dsl-data-product/src/components/DataProduct/DataProductDataAccess.tsx
@@ -106,7 +106,7 @@ import {
 } from '../../stores/ProductViewerNavigation.js';
 import { DataContractViewerState } from '../../stores/DataProduct/EntitlementsDataContractViewerState.js';
 import { EntitlementsDataContractCreator } from './DataContract/EntitlementsDataContractCreator.js';
-import { EntitlementsDataContractViewer } from './DataContract/EntitlementsDataContractViewer.js';
+import { DataAccessRequestViewer } from './DataContract/EntitlementsDataContractViewer.js';
 import { DataProductSubscriptionViewer } from './Subscriptions/DataProductSubscriptionsViewer.js';
 import {
   type DataProductAPGState,
@@ -1371,14 +1371,14 @@ export const DataProductAccessPointGroupViewer = observer(
           />
         )}
         {entitlementsDataContractViewerState && dataAccessState && (
-          <EntitlementsDataContractViewer
+          <DataAccessRequestViewer
             open={true}
             onClose={() =>
               dataAccessState.setContractViewerContractAndSubscription(
                 undefined,
               )
             }
-            currentViewer={entitlementsDataContractViewerState}
+            viewerState={entitlementsDataContractViewerState}
             onRefresh={() => {
               if (apgState.associatedUserContract) {
                 apgState.fetchUserAccessStatus(
@@ -1388,7 +1388,7 @@ export const DataProductAccessPointGroupViewer = observer(
                 );
               }
             }}
-            getContractTaskUrl={dataAccessState.getContractTaskUrl}
+            getTaskUrl={dataAccessState.getContractTaskUrl}
             getDataProductUrl={dataAccessState.getDataProductUrl}
           />
         )}
